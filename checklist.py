8b25b3ed22e771e84a025de99dfddf674d52ba57
abdul <abdul@abduls-MacBook-Pro.local>
2019-09-03 03:25:59 -0700

    first commit

---
+++ checklist.py
@@ -0,0 +1,45 @@
+print("Hello World")
+checklist = ["goclass", "doHW"]
+
+
+'''
+Create
+Read
+Update
+Delete
+'''
+
+
+
+# Create
+# def create(item):
+#     checklist.append(item)
+#
+#
+# create("eat food")
+
+checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+
+def create(item):
+    checklist.append('Blue')
+
+def create(item):
+    checklist.append(item)
+
+# READ
+def read(index):
+    item = checklist[index]
+    return item
+
+git init
+git add .
+git commit -m "first commit"
+
+
+print(checklist)
+
+
+print(checklist)
